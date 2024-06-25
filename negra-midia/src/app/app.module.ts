948987210d6345eb22201a7e7a7e3d608bd72ccf
Hedgar Bezerra <hedgarbezerra35@outlook.com>
2024-06-25 15:12:46 -0300

    ok

--- negra-midia/src/app/app.module.ts
+++ negra-midia/src/app/app.module.ts
@@ -21,9 +21,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   imports: [
     BrowserModule, CommonModule, RouterOutlet, IMaskModule, FormsModule, ReactiveFormsModule, HttpClientModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', {
   enabled: !isDevMode(),
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
+  registrationStrategy: 'registerImmediately'
 })
   ],
   declarations: [AppComponent, FooterComponent, NavbarComponent, HeaderComponent, ServicesComponent, AboutComponent, AboutItemComponent, LoadingSpinnerComponent],
